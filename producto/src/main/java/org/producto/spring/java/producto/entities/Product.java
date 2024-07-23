dffe66aea249b95cef9384ae0fadb7e95ee5b802
Aprendiz <Aprendiz@BOGDFPCGMF4164.FormacionSena.RED>
2024-07-23 15:39:37 -0500

    configuracion de bd

--- producto/src/main/java/org/producto/spring/java/producto/entities/Product.java
+++ producto/src/main/java/org/producto/spring/java/producto/entities/Product.java
@@ -1,13 +1,18 @@
 package org.producto.spring.java.producto.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
-
+@Builder
+@Entity
+@Table(name = "products")
 public class Product {
 
     private Long id;
